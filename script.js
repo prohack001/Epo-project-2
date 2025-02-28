b84c193177593f7bc1a21055b984b95ce796cb83
prohack001 <kiefferschlegel@gmail.com>
2025-02-28 16:44:31 +0000

    Updated HTML and JavaScript for new features

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.getElementById('welcomeButton').addEventListener('click', function () {
     document.getElementById('welcomeMessage').textContent = "Nous sommes ravis de vous accueillir sur le site dédié à l\'EPO. Explorez nos programmes et nos initiatives!";
 });
+
+// Ajoutez cette fonction en bas de votre script.js
+function changeDynamicContent() {
+    document.getElementById('dynamicContent').textContent = 'Le contenu a été changé grâce à JavaScript!';
+}
+// Appelons cette fonction au chargement de la page
+window.onload = function () {
+    changeDynamicContent();
+};
